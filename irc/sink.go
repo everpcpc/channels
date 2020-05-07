cd363248104ecdb5e82173de65f40764be22ea6d
everpcpc <git@everpcpc.com>
2020-05-07 20:45:12 +0800

    fix break irc msg for max length

--- irc/sink.go
+++ irc/sink.go
@@ -1,7 +1,6 @@
 package irc
 
 import (
-	"fmt"
 	"strings"
 
 	"channels/storage"
@@ -15,13 +14,30 @@ type sink func(message)
 
 func messageSink(conn connection) func(msg *storage.Message) {
 	return func(msg *storage.Message) {
-		replacer := strings.NewReplacer("\n", " ", "\r", " ")
-		text := replacer.Replace(msg.Text)
-		msgToSend := cmdPrivMsg.
-			withPrefix(fmt.Sprintf("%s!%s", msg.From, msg.From)).
-			withParams(msg.GetTarget()).
-			withTrailing(text)
+		target := msg.GetTarget()
+		text := strings.NewReplacer("\n", " ", "\r", " ").Replace(msg.Text)
 
-		conn.send(msgToSend)
+		// :nick PRIVMSG #channel :text\r\n
+		// with a safety buffer of 10
+		maxLength := IRC_MAX_MESSAGE_LENGTH - len(msg.From) - len(target) - 14 - 10
+
+		send := func(s string) {
+			msgToSend := cmdPrivMsg.
+				withPrefix(msg.From).
+				withParams(target).
+				withTrailing(s)
+			conn.send(msgToSend)
+		}
+
+		if len(text) <= maxLength {
+			send(text)
+			return
+		}
+
+		chunks := len(text) / maxLength
+		for i := 0; i < chunks; i++ {
+			send(text[i*maxLength : (i+1)*maxLength])
+		}
+		send(text[chunks*maxLength:])
 	}
 }
