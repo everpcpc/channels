675255680c16e6678b035613b62de52266d92c26
everpcpc <git@everpcpc.com>
2020-05-07 20:12:05 +0800

    fix replace CRLF in message before send irc

--- irc/sink.go
+++ irc/sink.go
@@ -2,18 +2,25 @@ package irc
 
 import (
 	"fmt"
+	"strings"
 
 	"channels/storage"
+)
+
+const (
+	IRC_MAX_MESSAGE_LENGTH = 512
 )
 
 type sink func(message)
 
 func messageSink(conn connection) func(msg *storage.Message) {
 	return func(msg *storage.Message) {
+		replacer := strings.NewReplacer("\n", " ", "\r", " ")
+		text := replacer.Replace(msg.Text)
 		msgToSend := cmdPrivMsg.
 			withPrefix(fmt.Sprintf("%s!%s", msg.From, msg.From)).
 			withParams(msg.GetTarget()).
-			withTrailing(msg.Text)
+			withTrailing(text)
 
 		conn.send(msgToSend)
 	}
