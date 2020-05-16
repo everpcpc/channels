2a00626d6108014a0ea739f3640d7331f84eb058
everpcpc <git@everpcpc.com>
2020-05-16 17:16:05 +0800

    [web]slack: add url verification event

--- web/slack.go
+++ web/slack.go
@@ -1,9 +1,38 @@
 package web
 
-import "github.com/gin-gonic/gin"
+import (
+	"encoding/json"
+
+	"github.com/gin-gonic/gin"
+	"github.com/slack-go/slack/slackevents"
+)
 
 func (e *env) slackEvents(token string) func(*gin.Context) {
+	verifyer := slackevents.OptionVerifyToken(
+		&slackevents.TokenComparator{VerificationToken: token},
+	)
 	return func(c *gin.Context) {
-		// TODO:(everpcpc)
+		data, err := c.GetRawData()
+		if err != nil {
+			c.AbortWithStatusJSON(500, gin.H{"error": err.Error()})
+			return
+		}
+		event, err := slackevents.ParseEvent(json.RawMessage(data), verifyer)
+		if err != nil {
+			c.AbortWithStatusJSON(500, gin.H{"error": err.Error()})
+			return
+		}
+		switch event.Type {
+		case slackevents.URLVerification:
+			var r *slackevents.ChallengeResponse
+			err := json.Unmarshal(data, &r)
+			if err != nil {
+				c.AbortWithStatusJSON(500, gin.H{"error": err.Error()})
+				return
+			}
+			c.JSON(200, gin.H{"challenge": r.Challenge})
+		default:
+			c.Status(200)
+		}
 	}
 }
