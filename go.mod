855937544914143a07c3e62d5c59d0d829aed29e
qizhicheng <qizhicheng@douban.com>
2020-07-21 17:50:27 +0800

    add some rearrange

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-ldap/ldap/v3 v3.2.1
 	github.com/go-redis/redis/v7 v7.4.0
+	github.com/prometheus/alertmanager v0.21.0
 	github.com/sirupsen/logrus v1.6.0
 	github.com/slack-go/slack v0.6.5
 	github.com/spf13/cobra v1.0.0
--- web/alertmanager.go
+++ web/alertmanager.go
@@ -8,33 +8,11 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+    promtemplate "github.com/prometheus/alertmanager/template"
 
 	"channels/auth"
 	"channels/storage"
 )
-
-// alertManagerMessage from:
-// https://prometheus.io/docs/alerting/configuration/#webhook_config
-// https://github.com/prometheus/alertmanager/blob/66a0ed21bdb0720b4ba083d35acd6ae77fa7b0b5/template/template.go#L227
-type alertManagerMessage struct {
-	Version           string
-	GroupKey          string
-	Status            string
-	Receiver          string
-	GroupLabels       map[string]string
-	CommonLabels      map[string]string
-	CommonAnnotations map[string]string
-	ExternalURL       string
-	Alerts            []struct {
-		Status       string
-		Labels       map[string]string
-		Annotations  map[string]string
-		StartsAt     time.Time
-		EndsAt       time.Time
-		GeneratorURL string
-		Fingerprint  string
-	}
-}
 
 // webhookAlertManager handles request from alertmanager as a webhook
 func (s *Server) webhookAlertManager(c *gin.Context) {
@@ -54,14 +32,15 @@ func (s *Server) webhookAlertManager(c *gin.Context) {
 		return
 	}
 
-	var msg alertManagerMessage
+	var msg promtemplate.Data
 	if err := c.BindJSON(&msg); err != nil {
 		c.AbortWithStatusJSON(400, gin.H{"error": err.Error()})
 		return
 	}
-	text := fmt.Sprintf("%s [%s:%s] is %s: %s\n( %s )\n",
+    fmt.Println(msg)
+	text := fmt.Sprintf("%s [%s] is %s: %s\n( %s )\n",
 		getStatusEmoji(msg.Status),
-		msg.GroupLabels["alertname"], msg.Version,
+		msg.GroupLabels["alertname"],
 		msg.CommonLabels["severity"],
 		msg.CommonAnnotations["summary"],
 		msg.ExternalURL,
@@ -75,23 +54,26 @@ func (s *Server) webhookAlertManager(c *gin.Context) {
 	}
 	text += "labels{" + strings.Join(labels, ",") + "}"
 
-	contentTemplate, err := template.New("content").Parse(`[{{ .Status | toUpper }}{{ if eq .Status "firing" }}:{{ .Alerts.Firing | len }}{{ end }}] {{ .CommonLabels.alertname }} for {{ .CommonLabels.job }}
-     {{- if gt (len .CommonLabels) (len .GroupLabels) -}}
-       {{" "}}(
-       {{- with .CommonLabels.Remove .GroupLabels.Names }}
-         {{- range $index, $label := .SortedPairs -}}
-           {{ if $index }}, {{ end }}
-           {{- $label.Name }}="{{ $label.Value -}}"
-         {{- end }}
-       {{- end -}}
-       )
-     {{- end }}
-     {{ range .Alerts -}}
-     *Alert:* {{ .Annotations.title }}{{ if .Labels.severity }} - ` + "`{{ .Labels.severity }}`" + `{{ end }}
 
-     *Description:* {{ .Annotations.description }}
+    titleTemplate, err := template.New("title").Parse(`[{{ .Status }}{{ if eq .Status "firing" }}:{{ .Alerts.Firing | len }}{{ end }}] {{ .CommonLabels.alertname }} for {{ .CommonLabels.job }}
+      {{- if gt (len .CommonLabels) (len .GroupLabels) -}}
+        {{" "}}(
+        {{- with .CommonLabels.Remove .GroupLabels.Names }}
+          {{- range $index, $label := .SortedPairs -}}
+            {{ if $index }}, {{ end }}
+            {{- $label.Name }}="{{ $label.Value -}}"
+          {{- end }}
+        {{- end -}}
+        )
+      {{- end }}`)
+	if err != nil {
+		panic(err)
+	}
 
-     *Details:*
+	contentTemplate, err := template.New("content").Parse(`{{ range .Alerts -}}
+*Alert:* {{if .Annotations.title }}{{ .Annotations.title }} {{ else }}{{ .Labels.alertname}}{{ end }}{{ if .Labels.severity }} - ` + "`{{ .Labels.severity }}`" + `{{ end }}
+*Description:* {{ .Annotations.description }}
+*Details:*
        {{ range .Labels.SortedPairs }} • *{{ .Name }}:* ` + "`{{ .Value }}`" + `
        {{ end }}
      {{ end }}`)
@@ -100,22 +82,35 @@ func (s *Server) webhookAlertManager(c *gin.Context) {
 	}
 
 	var tpl bytes.Buffer
+	if err := titleTemplate.Execute(&tpl, msg); err != nil {
+        fmt.Println(err.Error())
+		c.AbortWithStatusJSON(500, gin.H{"error": err.Error()})
+		return
+	}
+
+	title := tpl.String()
+
 	if err := contentTemplate.Execute(&tpl, msg); err != nil {
+        fmt.Println(err.Error())
 		c.AbortWithStatusJSON(500, gin.H{"error": err.Error()})
 		return
 	}
 
 	markdown := tpl.String()
+    fmt.Println(markdown)
 	m := storage.Message{
 		Source:    storage.MessageSourceWebhook,
 		From:      caller.Name,
 		To:        caller.Caps[0],
+        Title:     title,
 		Text:      text,
+        Color:     "#2eb886",
 		Markdown:  markdown,
 		Timestamp: time.Now().UnixNano(),
 	}
 
 	if err := s.store.Save(&m); err != nil {
+        fmt.Println(err.Error())
 		c.AbortWithStatusJSON(500, gin.H{"error": err.Error()})
 		return
 	}
